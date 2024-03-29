1281b23d8e39e492fb4dc976b82b1ae9ada849be
phamquanghao290 <phamquanghao290@gmail.com>
2024-03-29 15:29:46 +0700

    relace

--- src/modules/mailler/entities/mailler.entity.ts
+++ src/modules/mailler/entities/mailler.entity.ts
@@ -1,1 +1,16 @@
-export class Mailler {}
+import { Product } from './../../product/entities/product.entity';
+import { User } from "src/modules/users/entities/user.entity";
+import { PrimaryGeneratedColumn, Column, Entity, ManyToOne, JoinColumn } from "typeorm";
+@Entity()
+export class Mailler {
+    @PrimaryGeneratedColumn()
+    emailId: number;
+
+    @ManyToOne(() => User, (user) => user.emails)
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+
+    @ManyToOne(() => Product, (product) => product.emails)
+    @JoinColumn({ name: 'product_id' })
+    product: Product;
+}
--- src/modules/mailler/mailler.module.ts
+++ src/modules/mailler/mailler.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MaillerService } from './mailler.service';
 import { MaillerController } from './mailler.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Mailler } from './entities/mailler.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Mailler])],
   controllers: [MaillerController],
   providers: [MaillerService],
 })

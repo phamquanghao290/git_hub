2ed59f50764518c1a3ed8d24e5bc10f040f08245
phamquanghao290 <phamquanghao290@gmail.com>
2024-03-29 14:49:17 +0700

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { User } from './modules/users/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
+import { Product } from './modules/product/entities/product.entity';
+import { Mailler } from './modules/mailler/entities/mailler.entity';
+import { MaillerModule } from './modules/mailler/mailler.module';
+import { ProductModule } from './modules/product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'githup',
+      entities: [User, Product, Mailler],
+      synchronize: true,
+    }),
+    UsersModule, MaillerModule, ProductModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

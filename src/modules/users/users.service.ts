7f4cdcabce6f00e7535b00d4e15b59b8578b73c4
dminh31 <vbducminh3101@gmail.com>
2024-04-01 08:37:54 +0700

    feature/api_user

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as argon2 from 'argon2';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectRepository(User) private readonly userRepository:Repository<User>) {}
+  async create(createUserDto: any) {
+   const user = await this.userRepository.findOne({ where: { email: createUserDto.email } });
+   if (user) {
+    throw new Error('User already exists');
+   }
+   return await this.userRepository.save(createUserDto);
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async login(userLogin:any){
+    const {email, password} = userLogin;
+    const user = await this.userRepository.findOne({ where: { email:email, password: password } });
+    if (!user) {
+      return null
+    }
+    return user
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findUserByEmail(email: string) {
+    return await this.userRepository.findOne({ where: { email:email } });
   }
 }
